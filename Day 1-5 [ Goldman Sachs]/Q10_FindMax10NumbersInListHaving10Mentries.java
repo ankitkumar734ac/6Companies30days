e412c5bfd1fec70b9eeb5421e1a4e33ec8ebfe2c
Ankit Kumar <ankitkumar734ac@gmail.com>
2022-01-06 22:33:02 +0530

    File Update

--- Day 1-5 [ Goldman Sachs]/Q10_FindMax10NumbersInListHaving10Mentries.java
+++ Day 1-5 [ Goldman Sachs]/Q10_FindMax10NumbersInListHaving10Mentries.java
@@ -1,7 +1,47 @@
+import java.util.*;
 class Q10_FindMax10NumbersInListHaving10Mentries{
 	
-
-   //I cannot solved this problem
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int ans[]=new int[10];
+		int n=sc.nextInt();
+		int nums[]=new int[n];
+		for(int i=0;i<n;i++)
+		    nums[i]=sc.nextInt();
+		for(int i=0;i<10;i++)
+		    ans[i]=nums[i];
+		for(int i=10/2-1;i>=0;i--)
+		    heapify(ans,i,10);
+		for(int i=10;i<n;i++)
+		{
+		    if(nums[i]>ans[0])
+		    {
+		        ans[0]=nums[i];
+		        heapify(ans,0,10);
+		    }
+		}
+		System.out.println("Max 10 elements are:\n");
+		Arrays.sort(ans);
+		for(int i=0;i<10;i++)
+		    System.out.println(ans[i]);
+	}
+	public static void heapify(int arr[],int i,int n)
+	{
+	    int large=i;
+	    int l=2*i+1;
+	    int r=2*i+2;
+	    if(l<n && arr[l]<arr[large])
+	        large=l;
+	    if(r<n && arr[r]<arr[large])
+	        large=r;
+	    if(large!=i)
+	    {
+	        int tmp=arr[i];
+	        arr[i]=arr[large];
+	        arr[large]=tmp;
+	        heapify(arr,large,n);
+	    }
+	}
 
 
 }
